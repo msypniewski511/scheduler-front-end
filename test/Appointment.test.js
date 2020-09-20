f467847fa703c33c8e17d4e3718af2853a8d2467
Maciej Sypniewski <msypniewski511@gmail.com>
2020-09-20 18:27:43 +0100

    Reafactoring two examples

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -3,23 +3,28 @@ import ReactDOM from 'react-dom';
 import { Appointment } from '../src/Appointment';
 
 describe('Appointment', () => {
+	let customer;
+	let container;
+	beforeEach(() => {
+		container = document.createElement('div');
+	});
+
+	const render = component => ReactDOM.render(component, container);
 	it('renders the customer first name', () => {
-		const customer = { firstName: 'Ashley' };
-		// const component = <Appointment customer={customer} />;
-		const container = document.createElement('div');
+		customer = { firstName: 'Ashley' };
+		// component = <Appointment customer={customer} />;
 		// document.body.appendChild(container);
 
-		ReactDOM.render(<Appointment customer={customer} />, container);
+		render(<Appointment customer={customer} />, container);
 		expect(container.textContent).toMatch('Ashley');
 	});
 
 	it('renders the customer first name', () => {
-		const customer = { firstName: 'Jordan' };
+		customer = { firstName: 'Jordan' };
 		const component = <Appointment customer={customer} />;
-		const container = document.createElement('div');
 		// document.body.appendChild(container);
 
-		ReactDOM.render(component, container);
+		render(component, container);
 		expect(container.textContent).toMatch('Jordan');
 	});
 });
